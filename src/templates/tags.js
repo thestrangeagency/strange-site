6a2f18899de45ba05c3c88fdb957ee3ece6607df
lucas <lucas@thestrangeagency.com>
2018-08-30 11:27:20 +0200

    style tag page

--- src/templates/tags.js
+++ src/templates/tags.js
@@ -11,7 +11,7 @@ class TagRoute extends Component {
     const postLinks = posts.map(post => (
       <li key={post.node.fields.slug}>
         <Link to={post.node.fields.slug}>
-          <h2 className='is-size-2'>{post.node.frontmatter.title}</h2>
+          <h2 className='blog-title has-text-primary'>{post.node.frontmatter.title}</h2>
         </Link>
       </li>
     ))
@@ -31,10 +31,10 @@ class TagRoute extends Component {
               className='column is-10 is-offset-1'
               style={{ marginBottom: '6rem' }}
             >
-              <h3 className='title is-size-4 is-bold-light'>{tagHeader}</h3>
-              <ul className='taglist'>{postLinks}</ul>
+              <h3 className='title is-bold-light'>{tagHeader}</h3>
+              <ul className='post-list'>{postLinks}</ul>
               <p>
-                <Link to='/tags/'>Browse all tags</Link>
+                <Link className='button is-link mt-1' to='/tags/'>Browse all tags</Link>
               </p>
             </div>
           </div>
